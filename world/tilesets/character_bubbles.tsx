2e032a30e96827850b9948c8e522f8d8b0974e1f
lordeidi <lordeidi@swordlord.com>
2021-01-22 17:33:20 +0100

    Missing tilesets (w/ animations)

--- world/tilesets/character_bubbles.tsx
+++ world/tilesets/character_bubbles.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="bubbles" tilewidth="32" tileheight="32" tilecount="11" columns="11">
  <image source="../../gfx/characters/bubble.png" trans="ff00ff" width="352" height="32"/>
- <tile id="0">
+ <tile id="0" type="bubble_blow">
   <animation>
    <frame tileid="0" duration="100"/>
    <frame tileid="1" duration="100"/>
@@ -10,14 +10,14 @@
    <frame tileid="4" duration="100"/>
   </animation>
  </tile>
- <tile id="4">
+ <tile id="4" type="bubble_bobble">
   <animation>
    <frame tileid="4" duration="100"/>
    <frame tileid="5" duration="100"/>
    <frame tileid="6" duration="100"/>
   </animation>
  </tile>
- <tile id="7">
+ <tile id="7" type="bubble_poof">
   <animation>
    <frame tileid="7" duration="100"/>
    <frame tileid="8" duration="100"/>
--- world/tilesets/common_items.tsx
+++ world/tilesets/common_items.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="items" tilewidth="32" tileheight="48" tilecount="53" columns="53">
  <image source="../../gfx/common/items/items.png" trans="ff00ff" width="1696" height="48"/>
- <tile id="0">
+ <tile id="0" type="coin_silver">
+  <properties>
+   <property name="name" value="silvercoin"/>
+  </properties>
   <animation>
    <frame tileid="0" duration="100"/>
    <frame tileid="1" duration="100"/>
@@ -9,7 +12,7 @@
    <frame tileid="3" duration="100"/>
   </animation>
  </tile>
- <tile id="4">
+ <tile id="4" type="coin_gold">
   <animation>
    <frame tileid="4" duration="100"/>
    <frame tileid="5" duration="100"/>
@@ -17,7 +20,7 @@
    <frame tileid="7" duration="100"/>
   </animation>
  </tile>
- <tile id="8">
+ <tile id="8" type="coin_poof">
   <animation>
    <frame tileid="8" duration="100"/>
    <frame tileid="9" duration="100"/>
@@ -25,7 +28,7 @@
    <frame tileid="11" duration="100"/>
   </animation>
  </tile>
- <tile id="12">
+ <tile id="12" type="gem_blue">
   <animation>
    <frame tileid="12" duration="100"/>
    <frame tileid="13" duration="100"/>
@@ -33,7 +36,7 @@
    <frame tileid="15" duration="100"/>
   </animation>
  </tile>
- <tile id="16">
+ <tile id="16" type="gem_green">
   <animation>
    <frame tileid="16" duration="100"/>
    <frame tileid="17" duration="100"/>
@@ -41,7 +44,7 @@
    <frame tileid="19" duration="100"/>
   </animation>
  </tile>
- <tile id="20">
+ <tile id="20" type="gem_red">
   <animation>
    <frame tileid="20" duration="100"/>
    <frame tileid="21" duration="100"/>
@@ -49,7 +52,7 @@
    <frame tileid="23" duration="100"/>
   </animation>
  </tile>
- <tile id="24">
+ <tile id="24" type="gem_poof">
   <animation>
    <frame tileid="24" duration="100"/>
    <frame tileid="25" duration="100"/>
@@ -57,7 +60,7 @@
    <frame tileid="27" duration="100"/>
   </animation>
  </tile>
- <tile id="28">
+ <tile id="28" type="potion_blue">
   <animation>
    <frame tileid="28" duration="100"/>
    <frame tileid="29" duration="100"/>
@@ -68,7 +71,7 @@
    <frame tileid="34" duration="100"/>
   </animation>
  </tile>
- <tile id="35">
+ <tile id="35" type="potion_red">
   <animation>
    <frame tileid="35" duration="100"/>
    <frame tileid="36" duration="100"/>
@@ -79,7 +82,7 @@
    <frame tileid="41" duration="100"/>
   </animation>
  </tile>
- <tile id="42">
+ <tile id="42" type="potion_green">
   <animation>
    <frame tileid="42" duration="100"/>
    <frame tileid="43" duration="100"/>
@@ -90,7 +93,7 @@
    <frame tileid="48" duration="100"/>
   </animation>
  </tile>
- <tile id="49">
+ <tile id="49" type="potion_poof">
   <animation>
    <frame tileid="49" duration="100"/>
    <frame tileid="50" duration="100"/>
