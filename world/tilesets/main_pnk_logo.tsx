92c38346ec4a022314eda1368194f267edd30e7d
lordeidi <lordeidi@swordlord.com>
2021-02-14 23:48:08 +0100

    Main screen shows Logo and content

--- world/tilesets/main_pnk_logo.tsx
+++ world/tilesets/main_pnk_logo.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="main_pnk_logo" tilewidth="256" tileheight="84" tilecount="1" columns="1">
- <image source="../../gfx/main/pnk_logo.png" trans="ff00ff" width="256" height="84"/>
+<tileset version="1.4" tiledversion="1.4.3" name="main_pnk_logo" tilewidth="224" tileheight="96" tilecount="1" columns="1">
+ <grid orientation="orthogonal" width="256" height="94"/>
+ <image source="../../gfx/main/pnk_logo.png" trans="ff00ff" width="224" height="96"/>
 </tileset>
