24a5fb03c06690dcc7ff60817a0e76b44db2f0a1
lordeidi <lordeidi@swordlord.com>
2021-02-14 18:11:37 +0100

    Introducing main screen UI

---
+++ world/tilesets/common_hud_ui.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="hud_ui" tilewidth="16" tileheight="16" tilecount="105" columns="15">
+ <image source="../../gfx/common/hud_ui.png" trans="ff00ff" width="240" height="112"/>
+</tileset>
---
+++ world/tilesets/main_pnk_logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="main_pnk_logo" tilewidth="256" tileheight="84" tilecount="1" columns="1">
+ <image source="../../gfx/main/pnk_logo.png" trans="ff00ff" width="256" height="84"/>
+</tileset>
