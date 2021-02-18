6995169744d7312898ec71f67e888e88ee288752
lordeidi <lordeidi@swordlord.com>
2021-02-18 23:45:25 +0100

    Introducing castle decorations aseprite original and menus tilemap

---
+++ world/tilesets/common_menus.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="common_menus" tilewidth="58" tileheight="32" tilecount="3" columns="1">
+ <image source="../../gfx/common/menus.png" trans="ff00ff" width="58" height="96"/>
+</tileset>
