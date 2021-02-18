7c9db1cef2a9ee37905ed345659d30a7f53b80b8
lordeidi <lordeidi@swordlord.com>
2021-02-18 23:32:10 +0100

    redone tilemaps (logo and castle decorations)

--- world/tilesets/main_pnk_logo.tsx
+++ world/tilesets/main_pnk_logo.tsx
@@ -1,5 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="main_pnk_logo" tilewidth="224" tileheight="96" tilecount="1" columns="1">
- <grid orientation="orthogonal" width="256" height="94"/>
- <image source="../../gfx/main/pnk_logo.png" trans="ff00ff" width="224" height="96"/>
+<tileset version="1.4" tiledversion="1.4.3" name="main_pnk_logo" tilewidth="218" tileheight="36" tilecount="1" columns="1">
+ <image source="../../gfx/main/pnk_logo.png" trans="ff00ff" width="218" height="36"/>
 </tileset>
