0236e4ad85ad4d38266aefe18ad1d0fd6d68fb0a
LordFilu <lordfilu@swordlord.com>
2021-02-27 19:44:36 +0100

    object alignement top left

--- world/tilesets/main_pnk_logo.tsx
+++ world/tilesets/main_pnk_logo.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="main_pnk_logo" tilewidth="218" tileheight="36" tilecount="1" columns="1">
+<tileset version="1.4" tiledversion="1.4.3" name="main_pnk_logo" tilewidth="218" tileheight="36" tilecount="1" columns="1" objectalignment="topleft">
  <image source="../../gfx/main/pnk_logo.png" trans="ff00ff" width="218" height="36"/>
 </tileset>
