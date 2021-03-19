d262654a8da56c63df0196ca0c29a487229a8529
lordeidi <lordeidi@swordlord.com>
2021-03-19 13:40:58 +0100

    introducing cannons and pigs (and boxed and bombs)

---
+++ world/tilesets/character_cannonsnpigs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="character_cannonsnpigs" tilewidth="32" tileheight="32" tilecount="18" columns="18">
+ <image source="../../gfx/characters/cannons_n_pigs.png" trans="ff00ff" width="576" height="32"/>
+</tileset>
---
+++ world/tilesets/common_items_bomb.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="common_items_bomb" tilewidth="64" tileheight="64" tilecount="11" columns="11">
+ <image source="../../gfx/common/items/bomb.png" trans="ff00ff" width="704" height="64"/>
+</tileset>
