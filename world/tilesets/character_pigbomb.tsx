f4adce9aa40b0dbe9aa60d4b3706307b5646c2dc
lordeidi <lordeidi@swordlord.com>
2021-03-22 16:24:26 +0100

    missed the picking_up

--- world/tilesets/character_pigbomb.tsx
+++ world/tilesets/character_pigbomb.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="1.4.3" name="character_pigbomb" tilewidth="32" tileheight="32" tilecount="25" columns="25" objectalignment="topleft">
  <image source="../../gfx/characters/pig_bomb.png" trans="ff00ff" width="800" height="32"/>
- <tile id="0" type="throwing">
+ <tile id="0" type="picking_up">
   <animation>
    <frame tileid="0" duration="100"/>
    <frame tileid="1" duration="100"/>
