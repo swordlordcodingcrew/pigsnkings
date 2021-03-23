9c4521b390e8ffa75529d31a87efa45ec0a6b017
lordeidi <lordeidi@swordlord.com>
2021-03-23 11:05:14 +0100

    cleaning up the crates

---
+++ world/tilesets/character_pigcrate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="character_pigcrate" tilewidth="32" tileheight="32" tilecount="36" columns="36" objectalignment="topleft">
+ <image source="../../gfx/characters/pig_crate.png" trans="ff00ff" width="1152" height="32"/>
+</tileset>
