e0d4c0465594e9b46af55466735b336bf0661601
lordeidi <lordeidi@swordlord.com>
2021-04-02 20:06:23 +0200

    Pig explosion

--- world/tilesets/character_pig.tsx
+++ world/tilesets/character_pig.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="gfx_pig" tilewidth="32" tileheight="32" tilecount="16" columns="16" objectalignment="topleft">
- <image source="../../gfx/characters/pig.png" trans="ff00ff" width="512" height="32"/>
+<tileset version="1.4" tiledversion="1.4.3" name="gfx_pig" tilewidth="32" tileheight="32" tilecount="34" columns="34" objectalignment="topleft">
+ <image source="../../gfx/characters/pig.png" trans="ff00ff" width="1088" height="32"/>
  <tile id="0" type="sleeping">
   <animation>
    <frame tileid="0" duration="100"/>
@@ -31,6 +31,37 @@
   <animation>
    <frame tileid="14" duration="100"/>
    <frame tileid="15" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <animation>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="24">
+  <animation>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="26" duration="100"/>
+   <frame tileid="27" duration="100"/>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="33" duration="100"/>
   </animation>
  </tile>
 </tileset>
