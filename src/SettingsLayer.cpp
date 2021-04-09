3030b5d8190c96953ab3568c6434b53e4324949e
lordeidi <lordeidi@swordlord.com>
2021-04-09 14:37:10 +0200

    3d tests

--- src/SettingsLayer.cpp
+++ src/SettingsLayer.cpp
@@ -9,8 +9,12 @@
 #include "Imagesheet.hpp"
 
 #include "tinyraytracer.h" // see https://github.com/ssloy/tinyraytracer
+#include "type.h"
+#include "mesh_monkey.h"
 
 bool bRendered = false;
+
+#define FOV              64
 
 void raytrace() {
     Material      ivory(1.0, Vec4f(0.6,  0.3, 0.1, 0.0), Vec3f(0.4, 0.4, 0.3),   50.);
@@ -43,6 +47,272 @@ struct point {
 
 uint32_t framenum = 0;
 point ivorySphereCoords = { 0, -1.5, -14 };
+
+// ----------------------------------------------
+// global variables
+// ----------------------------------------------
+Matrix4 m_world;
+Vector3i mesh_rotation = {0, 0, 0};
+Vector3i mesh_position = {0, 0, 0};
+
+#define HALFW            160
+#define HALFH            120
+
+const unsigned int lut[] = {         // 0 to 90 degrees fixed point COSINE look up table
+        16384, 16381, 16374, 16361, 16344, 16321, 16294, 16261, 16224, 16182, 16135, 16082, 16025, 15964, 15897, 15825, 15749, 15668, 15582, 15491, 15395, 15295, 15190, 15081, 14967, 14848, 14725, 14598, 14466, 14329, 14188, 14043, 13894, 13740, 13582, 13420, 13254, 13084, 12910, 12732, 12550, 12365, 12175, 11982, 11785, 11585, 11381, 11173, 10963, 10748, 10531, 10310, 10086, 9860, 9630, 9397, 9161, 8923, 8682, 8438, 8191, 7943, 7691, 7438, 7182, 6924, 6663, 6401, 6137, 5871, 5603, 5334, 5062, 4790, 4516, 4240, 3963, 3685, 3406, 3126, 2845, 2563, 2280, 1996, 1712, 1427, 1142, 857, 571, 285, 0
+};
+
+#define LUT(a) (long)(&lut[a])// return value from LUT
+
+static int proj_nodes[NODECOUNT][2];         // projected nodes (x,y)
+static int old_nodes[NODECOUNT][2];          // projected nodes of previous frame to check if we need to redraw
+static unsigned char i;
+static int loops;
+static double next_tick;
+static double last_btn;                      // used for checking when the button was last pushed
+static unsigned char draw_type = 3;          // 0 - vertex | 1 - wireframe | 2 - flat colors | ...
+
+// ----------------------------------------------
+// SIN/COS from 90 degrees LUT
+// ----------------------------------------------
+long SIN(unsigned int angle) {
+    angle += 90;
+    if (angle > 450) return LUT(0);
+    if (angle > 360 && angle < 451) return -LUT(angle-360);
+    if (angle > 270 && angle < 361) return -LUT(360-angle);
+    if (angle > 180 && angle < 271) return  LUT(angle-180);
+    return LUT(180-angle);
+}
+
+long COS(unsigned int angle) {
+    if (angle > 360) return LUT(0);
+    if (angle > 270 && angle < 361) return  LUT(360-angle);
+    if (angle > 180 && angle < 271) return -LUT(angle-180);
+    if (angle > 90  && angle < 181) return -LUT(180-angle);
+    return LUT(angle);
+}
+
+// ----------------------------------------------
+// Matrix operation
+// ----------------------------------------------
+Matrix4 mMultiply(const Matrix4 &mat1, const Matrix4 &mat2) {
+    Matrix4 mat;
+    unsigned char r,c;
+    for (c=0; c<4; c++)
+        for (r=0; r<4; r++)
+            mat.m[c][r] = pMultiply(mat1.m[0][r], mat2.m[c][0]) +
+                          pMultiply(mat1.m[1][r], mat2.m[c][1]) +
+                          pMultiply(mat1.m[2][r], mat2.m[c][2]) +
+                          pMultiply(mat1.m[3][r], mat2.m[c][3]);
+    return mat;
+}
+
+Matrix4 mRotateX(const unsigned int angle) {
+    Matrix4 mat;
+    mat.m[1][1] =  COS(angle);
+    mat.m[1][2] =  SIN(angle);
+    mat.m[2][1] = -SIN(angle);
+    mat.m[2][2] =  COS(angle);
+    return mat;
+}
+
+Matrix4 mRotateY(const unsigned int angle) {
+    Matrix4 mat;
+    mat.m[0][0] =  COS(angle);
+    mat.m[0][2] = -SIN(angle);
+    mat.m[2][0] =  SIN(angle);
+    mat.m[2][2] =  COS(angle);
+    return mat;
+}
+
+Matrix4 mRotateZ(const unsigned int angle) {
+    Matrix4 mat;
+    mat.m[0][0] =  COS(angle);
+    mat.m[0][1] =  SIN(angle);
+    mat.m[1][0] = -SIN(angle);
+    mat.m[1][1] =  COS(angle);
+    return mat;
+}
+
+Matrix4 mTranslate(const long x, const long y, const long z) {
+    Matrix4 mat;
+    mat.m[3][0] =  x << PSHIFT;
+    mat.m[3][1] =  y << PSHIFT;
+    mat.m[3][2] =  z << PSHIFT;
+    return mat;
+}
+
+Matrix4 mScale(const float ratio) {
+    Matrix4 mat;
+    mat.m[0][0] *= ratio;
+    mat.m[1][1] *= ratio;
+    mat.m[2][2] *= ratio;
+    return mat;
+}
+
+// ----------------------------------------------
+// Shoelace algorithm to get the surface
+// ----------------------------------------------
+int shoelace(const int (*n)[2], const int index) {
+    unsigned char t = 0;
+    int surface = 0;
+    for (; t<3; t++) {
+        // (x1y2 - y1x2) + (x2y3 - y2x3) ...
+        surface += (n[faces[index][t]][0]           * n[faces[index][t<2?t+1:0]][1]) -
+                   (n[faces[index][t<2?t+1:0]][0] * n[faces[index][t]][1]);
+    }
+    return surface * 0.5;
+}
+
+// ----------------------------------------------
+// Shoelace algorithm for triangle visibility
+// ----------------------------------------------
+bool is_hidden(const int n[NODECOUNT][2], const int index) {
+    // (x1y2 - y1x2) + (x2y3 - y2x3) ...
+    return ( ( (n[faces[index][0]][0] * n[faces[index][1]][1]) -
+               (n[faces[index][1]][0] * n[faces[index][0]][1])   ) +
+             ( (n[faces[index][1]][0] * n[faces[index][2]][1]) -
+               (n[faces[index][2]][0] * n[faces[index][1]][1])   ) +
+             ( (n[faces[index][2]][0] * n[faces[index][0]][1]) -
+               (n[faces[index][0]][0] * n[faces[index][2]][1])   ) ) < 0 ? false : true;
+}
+
+// ----------------------------------------------
+// draw projected nodes
+// ----------------------------------------------
+void draw_vertex(const int (*n)[2], const uint16_t color) {
+    i = NODECOUNT-1;
+    do {
+        blit::Pen backgroundColour = blit::Pen(255, 255, 255, 255);
+        blit::screen.pen = backgroundColour;
+        blit::screen.pixel(blit::Point(n[i][0],n[i][1]));
+
+        //TFT.drawPixel(n[i][0],n[i][1], color);
+    } while(i--);
+}
+
+// ----------------------------------------------
+// draw edges between projected nodes
+// ----------------------------------------------
+void draw_wireframe(const int n[NODECOUNT][2], const uint16_t color) {
+    i = TRICOUNT-1;
+    do {
+        // don't draw triangle with negative surface value
+        if (!is_hidden(n, i)) {
+            blit::Pen backgroundColour = blit::Pen(255, 20, 255, 255);
+            blit::screen.pen = backgroundColour;
+            // draw triangle edges - 0 -> 1 -> 2 -> 0
+            blit::screen.line(blit::Point(n[faces[i][0]][0], n[faces[i][0]][1]), blit::Point(n[faces[i][1]][0], n[faces[i][1]][1]));
+            blit::screen.line(blit::Point(n[faces[i][1]][0], n[faces[i][1]][1]), blit::Point(n[faces[i][2]][0], n[faces[i][2]][1]));
+            blit::screen.line(blit::Point(n[faces[i][2]][0], n[faces[i][2]][1]), blit::Point(n[faces[i][0]][0], n[faces[i][0]][1]));
+            /*
+            TFT.drawLine(n[EDGE(i,0)][0], n[EDGE(i,0)][1], n[EDGE(i,1)][0], n[EDGE(i,1)][1], color);
+            TFT.drawLine(n[EDGE(i,1)][0], n[EDGE(i,1)][1], n[EDGE(i,2)][0], n[EDGE(i,2)][1], color);
+            TFT.drawLine(n[EDGE(i,2)][0], n[EDGE(i,2)][1], n[EDGE(i,0)][0], n[EDGE(i,0)][1], color);
+             */
+        }
+    } while(i--);
+}
+
+// ----------------------------------------------
+// draw flat color (not flat shading)
+// ----------------------------------------------
+void draw_flat_color(const int (*n)[2], uint16_t color) {
+    i = TRICOUNT-1;
+    int surface;
+    uint16_t col = color;
+    do {
+        // draw only triangles facing us
+        if ((surface=shoelace(n, i)) < 0) {
+            // this is an ugly hack but it 'somehow' fakes shading
+            // depending on the size of the surface of the triangle
+            // change the color toward brighter/darker
+            color = col * (surface * 0.001);
+
+            /*
+            TFT.fillTriangle(n[EDGE(i,0)][0], n[EDGE(i,0)][1],
+                             n[EDGE(i,1)][0], n[EDGE(i,1)][1],
+                             n[EDGE(i,2)][0], n[EDGE(i,2)][1],
+                             color);
+                             */
+        }
+    } while(i--);
+}
+
+// ----------------------------------------------
+// main loop
+// ----------------------------------------------
+void do_render() {
+    loops = 0;
+
+
+        // rotation
+        m_world = mRotateX(mesh_rotation.x);
+        m_world = mMultiply(mRotateY(mesh_rotation.y), m_world);
+        m_world = mMultiply(mRotateZ(mesh_rotation.z), m_world);
+        // scaling
+        //m_world = mMultiply(mScale(1.5), m_world);
+
+        // project nodes with world matrix
+        Vector3i p;
+        for (i=0; i<NODECOUNT; i++) {
+            p.x = (m_world.m[0][0] * (nodes[i][0] >> PSHIFT) +
+                   m_world.m[1][0] * (nodes[i][1] >> PSHIFT) +
+                   m_world.m[2][0] * (nodes[i][2] >> PSHIFT) +
+                   m_world.m[3][0]) / PRES;
+
+            p.y = (m_world.m[0][1] * (nodes[i][0] >> PSHIFT) +
+                   m_world.m[1][1] * (nodes[i][1] >> PSHIFT) +
+                   m_world.m[2][1] * (nodes[i][2] >> PSHIFT) +
+                   m_world.m[3][1]) / PRES;
+
+            p.z = (m_world.m[0][2] * (nodes[i][0] >> PSHIFT) +
+                   m_world.m[1][2] * (nodes[i][1] >> PSHIFT) +
+                   m_world.m[2][2] * (nodes[i][2] >> PSHIFT) +
+                   m_world.m[3][2]) / PRES;
+
+            // store projected node
+            proj_nodes[i][0] = (FOV * p.x) / (FOV + p.z) + HALFW;
+            proj_nodes[i][1] = (FOV * p.y) / (FOV + p.z) + HALFH;
+        }
+
+        // default auto-rotation mode
+        mesh_rotation.x+=3;
+        mesh_rotation.y+=2;
+        mesh_rotation.z++;
+
+        if (mesh_rotation.x > 360) mesh_rotation.x = 0;
+        if (mesh_rotation.y > 360) mesh_rotation.y = 0;
+        if (mesh_rotation.z > 360) mesh_rotation.z = 0;
+        // ...
+        loops++;
+
+
+    // ===============
+    // draw
+    // ===============
+    // render frame
+    switch(draw_type) {
+        case 0:
+            draw_vertex(old_nodes, 0);
+            draw_vertex(proj_nodes, 0);
+            break;
+        case 1:
+            draw_wireframe(old_nodes, 0);
+            draw_wireframe(proj_nodes, 0);
+            break;
+        case 2:
+            draw_flat_color(proj_nodes, 0);
+            break;
+        case 3:
+            draw_flat_color(proj_nodes, 0);
+            draw_wireframe(proj_nodes, 0);
+            break;
+        case 4:
+            draw_flat_color(proj_nodes, 0);
+            break;
+    }
+}
 
 namespace pnk
 {
@@ -174,9 +444,10 @@ namespace pnk
         blit::screen.pen = {200, 20, 200, 255};
         blit::screen.pixel(blit::Point(10,10));
 
-        if(bRendered) return;
+//        if(bRendered) return;
 
-        raytrace();
+        //raytrace();
+        do_render();
 
         bRendered = true;
 
