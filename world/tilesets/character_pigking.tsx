10553d011b8daaa54bac75e2dcf69d478d59a050
lordeidi <lordeidi@swordlord.com>
2021-08-20 17:15:57 +0200

    fixed animation names

--- world/tilesets/character_pigking.tsx
+++ world/tilesets/character_pigking.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="character_pigking" tilewidth="32" tileheight="32" tilecount="32" columns="32" objectalignment="topleft">
- <image source="../../gfx/characters/pig_king.png" trans="ff00ff" width="1065" height="32"/>
- <tile id="0">
+<tileset version="1.4" tiledversion="1.4.3" name="character_pigking" tilewidth="32" tileheight="32" tilecount="33" columns="33" objectalignment="topleft">
+ <image source="../../gfx/characters/pig_king.png" trans="ff00ff" width="1056" height="32"/>
+ <tile id="0" type="sleeping">
   <animation>
    <frame tileid="0" duration="100"/>
    <frame tileid="1" duration="100"/>
@@ -17,7 +17,7 @@
    <frame tileid="11" duration="100"/>
   </animation>
  </tile>
- <tile id="12">
+ <tile id="12" type="running">
   <animation>
    <frame tileid="12" duration="100"/>
    <frame tileid="13" duration="100"/>
@@ -27,14 +27,14 @@
    <frame tileid="17" duration="100"/>
   </animation>
  </tile>
- <tile id="17">
+ <tile id="17" type="landing">
   <animation>
    <frame tileid="18" duration="100"/>
    <frame tileid="19" duration="100"/>
    <frame tileid="20" duration="100"/>
   </animation>
  </tile>
- <tile id="21">
+ <tile id="21" type="jumping">
   <animation>
    <frame tileid="21" duration="100"/>
    <frame tileid="22" duration="100"/>
@@ -43,13 +43,13 @@
    <frame tileid="25" duration="100"/>
   </animation>
  </tile>
- <tile id="26">
+ <tile id="26" type="hit">
   <animation>
    <frame tileid="26" duration="100"/>
    <frame tileid="27" duration="100"/>
   </animation>
  </tile>
- <tile id="28">
+ <tile id="28" type="die">
   <animation>
    <frame tileid="28" duration="100"/>
    <frame tileid="29" duration="100"/>
