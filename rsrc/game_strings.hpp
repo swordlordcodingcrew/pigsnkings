ade7ce436aa824fdd708b4bd5a2416a59292eae7
LordFilu <lordfilu@swordlord.com>
2021-09-22 20:17:14 +0200

    static strings depot

---
+++ rsrc/game_strings.hpp
@@ -0,0 +1,10 @@
+// (c) 2019-21 by SwordLord - the coding crew
+// This file is part of the pnk game
+
+#pragma once
+
+
+namespace pnk
+{
+    const static std::string_view str_lvl1_intro{"You enter your chambers\nin the search for The Lost Axe\nBut alas, what do you see??\nPigs all around!"};
+}
