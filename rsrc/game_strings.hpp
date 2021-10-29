8a9f27e7aab5cd6589022a01a451f3e81d4ff8da
lordeidi <lordeidi@swordlord.com>
2021-10-29 17:45:38 +0200

    Push the button! (X)

--- rsrc/game_strings.hpp
+++ rsrc/game_strings.hpp
@@ -6,6 +6,15 @@
 
 namespace pnk
 {
+/**
+ * Generic strings
+ */
+const static std::string_view str_game_over{"OH NO!\n\
+You lost.\n\
+\n\
+Game Over!\n\
+\n\
+(press 'X' to quit)"};
 
 /**
  * Level 1 strings
@@ -15,19 +24,12 @@ in the search for The Lost Axe\n\
 But alas, what do you see??\n\
 Pigs all around!\n\
 \n\
-(press 'B' to start)"};
+(press 'X' to start)"};
 
 const static std::string_view str_lvl1_end{"The pig boss is\n\
 vanquished!\n\
 Lets clean up the next floor\n\
 You may proceed to the door"};
-
-const static std::string_view str_game_over{"OH NO!\n\
-\n\
-You lost.\n\
-Game Over!\n\
-\n\
-(press 'B' to quit)"};
 
 /**
  * Level 2 strings
@@ -38,7 +40,7 @@ in the 2nd floor\n\
 But AAARGH\n\
 Even more pigs around!\n\
 \n\
-(press 'B' to start)"};
+(press 'X' to start)"};
 
 const static std::string_view str_lvl2_end{"The 2nd floor\n\
 pig boss is vanquished!\n\
