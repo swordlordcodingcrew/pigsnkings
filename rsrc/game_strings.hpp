be109094cc215ff0a96a16289f1dc7ee0595876e
lordeidi <lordeidi@swordlord.com>
2021-10-30 13:57:57 +0200

    Introducing hack to leave game after pig king from level 1 was killed

--- rsrc/game_strings.hpp
+++ rsrc/game_strings.hpp
@@ -26,10 +26,23 @@ Pigs all around!\n\
 \n\
 (press 'X' to start)"};
 
-const static std::string_view str_lvl1_end{"The pig boss is\n\
+/*
+ * We will replace below text with that here once we have level 2!
+ const static std::string_view str_lvl1_end{"The pig boss is\n\
 vanquished!\n\
 Lets clean up the next floor\n\
 You may proceed to the door"};
+
+ */
+// TODO replace me with the text above once we have level 2
+const static std::string_view str_lvl1_end{"The pig boss is\n\
+vanquished!\n\
+\n\
+But let us first design\n\
+the next few levels\n\
+\n\
+You will now return\n\
+to the main screen"};
 
 /**
  * Level 2 strings
